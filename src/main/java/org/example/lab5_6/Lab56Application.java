35a2c0efd3099c4600b0b2ab8928cc2df7bf0bb7
BiancaZO <89614618+BiancaZO@users.noreply.github.com>
2024-08-05 20:22:41 -0700

    First commit

---
+++ src/main/java/org/example/lab5_6/Lab56Application.java
@@ -0,0 +1,35 @@
+package org.example.lab5_6;
+
+import org.example.lab5_6.entities.Student;
+import org.example.lab5_6.repositories.StudentRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class Lab56Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Lab56Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            studentRepository.save(new Student(null, "Jam", new Date(), false, 1.2 ));
+            studentRepository.save(new Student(null, "Jen", new Date(), true, 3.2));
+            studentRepository.save(new Student(null, "Jok", new Date(), false, 1.0));
+            studentRepository.save(new Student(null, "Jos", new Date(), false, 4.2));
+            studentRepository.findAll().forEach(p -> {
+                System.out.println(p.getName());
+            });
+        };
+    }
+
+}
---
+++ src/main/java/org/example/lab5_6/repositories/StudentRepository.java
@@ -0,0 +1,16 @@
+package org.example.lab5_6.repositories;
+
+import org.example.lab5_6.entities.Student;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface StudentRepository extends JpaRepository<Student,Long> {
+    List<Student> findStudentById (long kw);
+
+
+
+}
